715145ae0163d7d01eaaab64e68f4fd4ab275f27
Volochkovskii Anton <volochkovskij@realweb.ru>
2019-04-28 02:10:46 +0300

    META-1972 [+] Добавлен метод query для запросов к БД

---
+++ dbquery.go
@@ -0,0 +1,32 @@
+package metasdk
+
+import (
+	"encoding/json"
+	"log"
+)
+
+func (m *Meta) query(options map[string]string, command string, maxRows int, parameters ...map[string]string) []map[string]interface{} {
+	dbQuery := dbQuery{}
+	dbQuery.Database = map[string]string{
+		"alias": options["dbAlias"],
+	}
+	dbQuery.DbQuery = map[string]interface{}{
+		"maxRows":    maxRows,
+		"command":    command,
+		"parameters": parameters,
+		"shardKey":   options["shardKey"],
+	}
+
+	req, err := json.Marshal(dbQuery)
+	check(err)
+
+	resp := m.nativeCall("db", "query", "POST", req)
+	if resp == nil {
+		log.Panic("Произошла ошибка при запросе в БД")
+	}
+
+	dbResponse := dbResponse{}
+	check(json.Unmarshal(resp, &dbResponse))
+
+	return dbResponse.Rows
+}
